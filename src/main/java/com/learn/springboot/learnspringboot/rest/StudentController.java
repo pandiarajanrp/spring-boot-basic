9341781a38eb5d60417224500a276aee8e678598
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-11 19:46:49 -0600

    exception handling

--- src/main/java/com/learn/springboot/learnspringboot/rest/StudentController.java
+++ src/main/java/com/learn/springboot/learnspringboot/rest/StudentController.java
@@ -1,23 +1,49 @@
 package com.learn.springboot.learnspringboot.rest;
 
 import com.learn.springboot.learnspringboot.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learn.springboot.learnspringboot.error.StudentErrorResponse;
+import com.learn.springboot.learnspringboot.error.StudentNotFoundException;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/students")
 public class StudentController {
-    @GetMapping()
-    public List<Student> getCustomer() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    private List<Student> loadStudents() {
+        students = new ArrayList<>();
         students.add(new Student("Pandiarajan", "Rajagopal", "pandiarajan.rp@gmail.com"));
         students.add(new Student("John", "Smith", "john.smith@gmail.com"));
         students.add(new Student("Pablo", "Marty", "pablo.marty@gmail.com"));
         return students;
+    }
+    @GetMapping()
+    public List<Student> getCustomer() {
+        return students;
+    }
+    @GetMapping("/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        // System.out.println("Student ID " + studentId);
+        if(studentId < 0 || studentId > students.size()) {
+            throw new StudentNotFoundException("Student not found with ID " + studentId);
+        }
+        return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exec) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exec.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
 
