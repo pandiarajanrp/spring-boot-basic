f5f2b83e66989d2799e07ac262e890a378c69e4b
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-11 19:57:10 -0600

    @ControllerAdvice

--- src/main/java/com/learn/springboot/learnspringboot/rest/StudentController.java
+++ src/main/java/com/learn/springboot/learnspringboot/rest/StudentController.java
@@ -1,11 +1,8 @@
 package com.learn.springboot.learnspringboot.rest;
 
 import com.learn.springboot.learnspringboot.entity.Student;
-import com.learn.springboot.learnspringboot.error.StudentErrorResponse;
 import com.learn.springboot.learnspringboot.error.StudentNotFoundException;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -35,24 +32,6 @@ public class StudentController {
             throw new StudentNotFoundException("Student not found with ID " + studentId);
         }
         return students.get(studentId);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exec) {
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exec.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exec) {
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage("Bad Request");
-        error.setTimestamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
 
