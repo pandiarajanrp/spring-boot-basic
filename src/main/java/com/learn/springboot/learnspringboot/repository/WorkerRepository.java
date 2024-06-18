9ce53ad88d8e5fd554f72a609a0d9c420eb72be7
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-18 17:58:09 -0600

    spring boot data rest path options and properties

--- src/main/java/com/learn/springboot/learnspringboot/repository/WorkerRepository.java
+++ src/main/java/com/learn/springboot/learnspringboot/repository/WorkerRepository.java
@@ -2,6 +2,10 @@ package com.learn.springboot.learnspringboot.repository;
 
 import com.learn.springboot.learnspringboot.entity.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// By default it exposes the endpoint with workers path
+// @RepositoryRestResource provides an option to change the path name
+@RepositoryRestResource(path = "members")
 public interface WorkerRepository extends JpaRepository<Worker, Integer> {
 }
